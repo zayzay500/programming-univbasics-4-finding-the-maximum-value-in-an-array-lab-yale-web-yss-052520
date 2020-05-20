bb63fa508ec2a2ecd460e8e16f5238903815726e
Lindsay Pierce <lindsay.pierce@yale.edu>
2020-05-20 00:33:43 +0000

    Done.

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,10 @@
 def find_max_value(array)
-  # Add your solution here
+  max_value = 0
+  array.length.times do |index|
+    value = array[index]
+    if value > max_value
+      max_value = value
+    end
+  end
+  max_value
 end
